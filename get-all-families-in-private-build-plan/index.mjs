e1bd11f35a3132f9c49d0f6219f951dc6b0fb593
be5invis <belleve@typeof.net>
2026-04-20 18:10:40 -0700

    Refine the plan sample, also comment TTC options to test the empty job case

--- get-all-families-in-private-build-plan/index.mjs
+++ get-all-families-in-private-build-plan/index.mjs
@@ -6,8 +6,8 @@ const tomlContent = fs.readFileSync(tomlPath, "utf-8");
 const parsedToml = toml.parse(tomlContent);
 
 process.stdout.write(
-    "buildPlans=" + JSON.stringify(Object.keys(parsedToml.buildPlans)) + "\n"
+    "buildPlans=" + JSON.stringify(Object.keys(parsedToml.buildPlans || {})) + "\n"
 );
 process.stdout.write(
-    "collectPlans=" + JSON.stringify(Object.keys(parsedToml.collectPlans)) + "\n"
+    "collectPlans=" + JSON.stringify(Object.keys(parsedToml.collectPlans || {})) + "\n"
 );
